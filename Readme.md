be38a1505d7148c2c3d6c72d5276038003f692fa
aslampangestu <aslampangestu@gmail.com>
2021-02-13 20:26:36 +0700

    done basic

--- Readme.md
+++ Readme.md
@@ -1,8 +1,20 @@
-2 Tipe Package
+#2 Tipe Package
 1. Executable -> file yang akan dieksekusi. Harus menggunakan main
 2. Liblary -> Dipanggil di file lain untuk diekskusi
 
-Jenis Import
+#Jenis Import
 1. Import standart lib golang
 2. Package yang kita bikin sendiri
 3. Dibikin orang lain
+
+#Variabel Definition
+
+var "variabel_name" "data_type" -> Default Value
+var "variabel_name" "data_type" = "isinya" -> With assigment
+
+"variabel_name" := "isinya"
+
+#Default Value
+String -> ""
+Integer -> 0
+
--- main.go
+++ main.go
@@ -7,11 +7,74 @@ import (
 
 func main() {
 	fmt.Println("Test Print")
-	sentence := TestFucn()
+	// sentence := TestFucn()
+	// fmt.Println(sentence)
 
 	result := calculation.Add(3, 6)
-
 	fmt.Println(result)
 
-	fmt.Println(sentence)
+	//Variable Definition
+	var name string = "hai"
+	var name1 string
+	name1 = "Cek ssatu"
+	age := 30
+	fmt.Println(name)
+	fmt.Println(name1)
+	fmt.Println(age)
+
+	//Conditional IF
+	umur := 10
+	score := 80
+	var grade string
+	if umur > 10 {
+		fmt.Println("Boleh main game")
+	}
+
+	if umur > 10 {
+		fmt.Println("Boleh main game")
+	} else {
+		fmt.Println("Tidak Boleh main game")
+	}
+
+	if score <= 50 {
+		grade = "E"
+	} else if score <= 60 {
+		grade = "D"
+	} else {
+		grade = "A"
+	}
+	fmt.Println(grade)
+
+	//Conditional Switch
+	number := 5
+	switch number {
+	case 1:
+		fmt.Println("Sate")
+	case 2:
+		fmt.Println("Dua")
+	default:
+		fmt.Println("Nukk")
+	}
+
+	//Looping
+	index := 1
+	title := "OI AH UW LU"
+	//For
+	for i := 1; i <= 5; i++ {
+		fmt.Println("KWKWKWKW", i)
+	}
+	//While
+	for index < 5 {
+		fmt.Println("KWKWKWKW", index)
+		index++
+	}
+	//FOREACH with index
+	for index, letter := range title {
+		fmt.Println(index, string(letter))
+	}
+
+	//FOREACH without index
+	for _, letter := range title {
+		fmt.Println(string(letter))
+	}
 }
